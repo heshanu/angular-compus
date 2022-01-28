8dc27e5ef95adcac3b62c60a0ee8f1c3a8b8dd8c
heshan umayanga <heshanu97@gmail.com>
2022-01-28 23:05:14 +0530

    Adddd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import { ElementsHomeComponent } from './elements/elements-home/elements-home.component';
 
 const routes: Routes = [];
 
