097683e9b45463a09e9507254deef57033bae9d6
heshan umayanga <heshanu97@gmail.com>
2022-01-29 00:00:17 +0530

    Added routing semantic lib in app.compoent.html

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -13,9 +13,6 @@ import { CollectionsHomeComponent } from './collections-home/collections-home.co
   imports: [
     CommonModule,
     CollectionsRoutingModule
-  ],
-  exports:[
-    CollectionsHomeComponent
   ]
 })
 export class CollectionsModule { }
