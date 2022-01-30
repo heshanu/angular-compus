50ef08591f91bf117fdaaeb46af6d5f2c3eead77
heshan umayanga <heshanu97@gmail.com>
2022-01-30 17:14:28 +0530

    added employed to collection home.compoent.html and collection.component.html

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,12 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
   data=[
-    {name:'hehab',age:24,job:'desiger'},
-    {name:'hehab',age:25,job:'desiger'},
-    {name:'hehab',age:26,job:'desiger'},
-    {name:'hehab',age:27,job:'desiger'}
+    {name:'hehab',age:24,job:'desiger',employed:true},
+    {name:'hehab',age:25,job:'desiger',employed:false},
+    {name:'hehab',age:26,job:'desiger',employed:true},
+    {name:'hehab',age:27,job:'desiger',employed:true}
   ];
   headers=[
+    {key:'employed',label:'Has a Job:?'},
     {key:'name',label:'Name'},
     {key:'age',label:'Age'},
     {key:'job',label:'Job'}
