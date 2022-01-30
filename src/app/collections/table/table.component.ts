3c6e3e6fb10b0a9cfb4432845ab3f3389f202150
heshan umayanga <heshanu97@gmail.com>
2022-01-30 17:29:42 +0530

    added table.compoents.ts and table.copoent.html added class called striped celled to style

--- src/app/collections/table/table.component.ts
+++ src/app/collections/table/table.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit,Input } from '@angular/core';
 export class TableComponent implements OnInit {
 
   @Input() data:any=[];
+  @Input() classNames='';
+  
   @Input()  headers:any=[];
   constructor() { }
 
