f545bff97ebdcc68dbb360dfdfe7c58da873dbf2
heshan umayanga <heshanu97@gmail.com>
2022-01-30 12:42:17 +0530

    Added input in table compoents

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,8 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
 
-  constructor() { }
+  data=[
+    {name:'hehab',age:24,job:'desiger'},
+    {name:'hehab',age:24,job:'desiger'},
+    {name:'hehab',age:24,job:'desiger'},
+    {name:'hehab',age:24,job:'desiger'},
+  ];
 
+  headers=[
+    {key:'name',label:'Name'},
+    {key:'age',label:'Age'},
+    {key:'job',label:'Job'}
+  ];
+
+  constructor() { }
+  
   ngOnInit(): void {
   }
 
